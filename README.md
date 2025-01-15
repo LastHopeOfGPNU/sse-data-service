0e740ad64cb855ee39fbb18e7e6bf4d30fa55e6f
lyx <28721054@qq.com>
2025-01-16 00:45:02 +0800

    add first api

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sse-data-service
-上海证券交易所公开发行公司债券（含企业债券）公告
+上海证券交易所公开发行公司债券（含企业债券）公告数据接口
+
+## Get started
+```bash
+pip install requirements.txt
+cd api
+uvicorn main:app --reload
+```
+
+## Request example
+```sh
+curl --location 'http://127.0.0.1:8000/?start_date=2025-01-01&end_date=2025-01-02&page=1'
+```
---
+++ api/main.py
@@ -0,0 +1,42 @@
+import json
+import httpx
+import datetime
+from fastapi import FastAPI
+
+
+app = FastAPI()
+
+
+@app.get("/")
+async def get_data(start_date: str, end_date: str, page: int = 1):
+    """
+    Args:
+        start_date : query start date in format "YYYY-MM-DD"
+        end_date: query end date (included) in format "YYYY-MM-DD"
+        page: page number
+    """
+    url = "https://query.sse.com.cn/commonSoaQuery.do"
+    start_date = datetime.datetime.strptime(start_date, "%Y-%m-%d")
+    end_date = datetime.datetime.strptime(end_date, "%Y-%m-%d")
+    params = {
+        "jsonCallBack": "jsonpCallback74421704",
+        "isPagination": True,
+        "pageHelp.pageSize": 25,
+        "pageHelp.cacheSize": 1,
+        "type": "inParams",
+        "sqlId": "BS_ZQ_GGLL",
+        "sseDate": start_date.strftime("%Y-%m-%d 00:00:00"),
+        "sseDateEnd": end_date.strftime("%Y-%m-%d 23:59:59"),
+        "bondType": "CORPORATE_BOND_BULLETIN,COMPANY_BOND_BULLETIN",
+        "order": "sseDate|desc,securityCode|asc,bulletinId|asc",
+        "pageHelp.pageNo": page,
+        "pageHelp.beginPage": page,
+        "pageHelp.endPage": page
+    }
+    headers = {
+        "referer": "https://www.sse.com.cn/"
+    }
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, params=params, headers=headers)
+        response = json.loads(response.content.decode().strip("jsonpCallback74421704()"))
+        return response["pageHelp"]
